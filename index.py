e4ef914bc0b25984df38f238588fc0eeef54d8b0
RudyDinis <60667200+RudyDinis@users.noreply.github.com>
2024-10-14 12:42:56 +0200

    Update index.py

--- index.py
+++ index.py
@@ -13,19 +13,19 @@ def charge_corpus(corpus_file):
     with open(corpus_file, encoding='utf-8') as file:
         for line in file:
             phrase = re.sub(r'[^\w\s]', '', line).split()
-            for indice in range(0, len(phrase)-1):
-
-                if phrase[indice] in dico:
-                    dico[phrase[indice]] += [[phrase[indice +1]]]
+            for indice in range(0, len(phrase)-2):
+                
+                if (phrase[indice] + " " + phrase[indice +1]) in dico:
+                    dico[phrase[indice] + " " + phrase[indice +1]] += [[phrase[indice +2]]]
                 else:
-                    dico[phrase[indice]] = [[phrase[indice +1]]]
+                    dico[phrase[indice] + " " + phrase[indice +1]] = [[phrase[indice + 2]]]
 
 def candidat(mot):
     '''
         On utilise la variable dico pour chercher qu'elle mot on été dit après le mot donné en argument pour les mettre dans la variable candidat_mot_suivant
     '''
     global candidat_mot_suivant
-    candidat_mot_suivant = {} #remet la variable a 0raboule un mot
+    candidat_mot_suivant = {} #remet la variable a 0 un mot
     for element in dico[mot]:
         if element[0] in candidat_mot_suivant:
             candidat_mot_suivant[element[0]] += 1
@@ -40,10 +40,9 @@ def choix_mot():
     poids = list(candidat_mot_suivant.values())
     mot_choisi = random.choices(mots, weights=poids)[0]# Choisi un mot au hasard en utilisant le poid pour faire une pondération
 
-
     return mot_choisi
 
-#charge_corpus("fra_news_2023_1M-sentences.txt")
+charge_corpus("fra_news_2023_1M-sentences.txt")
 
 def main(mot, nb_mot):
     '''
@@ -54,12 +53,10 @@ def main(mot, nb_mot):
 
     #Charger le corpus
     for _ in range(0, nb_mot-1):
-        candidat(dernier_mot)
+        candidat(dernier_mot) 
         dernier_mot = choix_mot()
         phrase += " " + dernier_mot
     return phrase
 
 
-print(int(4 / str('4')))
-
-
+print(main("les voitures", 2))
